16874111033525a34e57367e0d892119ea120140
chaeminyu <chaemin.u@gmail.com>
2024-11-21 11:07:01 +0900

    [Feat] Initial commit

---
+++ src/test/java/org/zerock/yuchaemin/imfinebackend/ImfineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.yuchaemin.imfinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImfineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
